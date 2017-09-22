bfff49d5b508bb12761bca70a250fac823d3b330
Dan Kiss <flowfast47@gmail.com>
2017-09-22 12:51:53 -0700

    add attribute reader for name of volunteer

---
+++ lib/volunteer.rb
@@ -0,0 +1,50 @@
+class Volunteer
+  attr_reader(:name, :project_id, :id)
+  def initialize(attributes)
+    @name = attributes.fetch(:name)
+    @project_id = attributes.fetch(:project_id)
+    @id = attributes.fetch(:id)
+  end
+
+ #  def save()
+ #    result = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
+ #    @id = result.first().fetch("id").to_i()
+ #  end
+ #
+ #  def ==(another_object)
+ # self.title().==(another_object.title).&(self.id().==(another_object.id()))
+ #  end
+ #
+ #  def self.all()
+ #    returned_projects = DB.exec("SELECT * FROM projects")
+ #    list_of_projects = []
+ #    returned_projects.each() do |single_project|
+ #      title = single_project.fetch("title")
+ #      id = single_project.fetch("id").to_i
+ #      the_project = Project.new({:title => title, :id => id})
+ #      list_of_projects.push(the_project)
+ #    end
+ #    list_of_projects
+ #  end
+ #
+ #  def self.find(project_id_to_find)
+ #    found_project = nil
+ #    Project.all().each do |single_project|
+ #      if single_project.id().==(project_id_to_find)
+ #         found_project = single_project
+ #      end
+ #    end
+ #    found_project
+ #  end
+ #
+
+
+
+
+
+
+
+
+
+
+end
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -68,14 +68,14 @@ describe Project do
   end
   #
   # describe '#volunteers' do
-  #   it 'returns all volunteers for a specific project' do
-  #     project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
-  #     project.save
-  #     volunteer1 = Volunteer.new({:name => 'Jasmine', :project_id => project.id, :id => nil})
-  #     volunteer1.save
-  #     volunteer2 = Volunteer.new({:name => 'Joe', :project_id => project.id, :id => nil})
-  #     volunteer2.save
-  #     expect(project.volunteers).to eq [volunteer1, volunteer2]
+    # it 'returns all volunteers for a specific project' do
+    #   project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
+    #   project.save
+    #   volunteer1 = Volunteer.new({:name => 'Jasmine', :project_id => project.id, :id => nil})
+    #   volunteer1.save
+    #   volunteer2 = Volunteer.new({:name => 'Joe', :project_id => project.id, :id => nil})
+    #   volunteer2.save
+    #   expect(project.volunteers).to eq [volunteer1, volunteer2]
   #   end
   # end
   #
