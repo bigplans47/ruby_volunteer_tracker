242000f6da2976b704faabda26291710d8650559
Dan Kiss <flowfast47@gmail.com>
2017-09-22 10:38:05 -0700

    add code to pass the save method

--- lib/project.rb
+++ lib/project.rb
@@ -1,8 +1,17 @@
 class Project
   attr_reader(:title, :id)
   def initialize(attributes)
-    @id = attributes.fetch(:id)
     @title = attributes.fetch(:title)
+    @id = attributes.fetch(:id)
+  end
+
+  def save
+    result = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
+    @id = result.first().fetch("id").to_i()
+  end
+
+  def ==(another_object)
+    self.title().==(another_object.title).&(self.id()).==(another_object.id())
   end
 
 
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -15,13 +15,13 @@ describe Project do
     end
   end
 
-  #   it 'returns the id of the project after saving project' do
-  #     project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
-  #     project.save
-  #     expect(project.id).to be_an_instance_of Integer
-  #   end
-  # end
-  #
+    it 'returns the id of the project after saving project' do
+      project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
+      project.save
+      expect(project.id).to be_an_instance_of Integer
+    end
+  # end -not needed
+
   # describe '#==' do
   #   it 'is the same project if two projects have the same title' do
   #     project1 = Project.new({:title => 'Teaching Kids to Code', :id => nil})
