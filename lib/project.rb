ed91ece0885e7c9299d43adaea591b4255826472
Dan Kiss <flowfast47@gmail.com>
2017-09-22 11:59:19 -0700

    add self.find method

--- lib/project.rb
+++ lib/project.rb
@@ -5,7 +5,7 @@ class Project
     @id = attributes.fetch(:id)
   end
 
-  def save
+  def save()
     result = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
     @id = result.first().fetch("id").to_i()
   end
@@ -14,7 +14,7 @@ class Project
  self.title().==(another_object.title).&(self.id().==(another_object.id()))
   end
 
-  def self.all
+  def self.all()
     returned_projects = DB.exec("SELECT * FROM projects")
     list_of_projects = []
     returned_projects.each() do |single_project|
@@ -25,5 +25,25 @@ class Project
     end
     list_of_projects
   end
+
+  def self.find(project_id_to_find)
+    found_project = nil
+    Project.all().each do |single_project|
+      if single_project.id().==(project_id_to_find)
+         found_project = single_project
+      end
+    end
+    found_project
+  end
+
+
+
+
+
+
+
+
+
+
 
 end
